97423f2eaf4146131bcdb006533782d9ba32b212
Vladimir Brankov <vbrankov@janestreet.com>
2015-05-19 10:58:33 -0400

    Finish h5lt examples.

--- lib/h5lt.mli
+++ lib/h5lt.mli
@@ -1,4 +1,20 @@
 open Bigarray
 
+module Dataset_info : sig
+  type t = {
+    dims      : Hsize.t array;
+    class_id  : H5t.Class.t;
+    type_size : int;
+  }
+end
+
 external make_dataset : Loc.t -> string -> Hsize.t array -> H5t.t -> _ Genarray.t -> unit
   = "hdf5_h5lt_make_dataset"
+external read_dataset_int : Loc.t -> string -> _ Genarray.t -> unit
+  = "hdf5_h5lt_read_dataset_int"
+external get_dataset_info : Loc.t -> string -> Dataset_info.t
+  = "hdf5_h5lt_get_dataset_info"
+external set_attribute_int : Loc.t -> string -> string -> (int32, int32_elt, _) Array1.t
+  -> unit = "hdf5_h5lt_set_attribute_int"
+external get_attribute_int : Loc.t -> string -> string -> (int32, int32_elt, _) Array1.t
+  -> unit = "hdf5_h5lt_get_attribute_int"
--- lib/h5lt_stubs.c
+++ lib/h5lt_stubs.c
@@ -1,3 +1,4 @@
+#include <caml/alloc.h>
 #include <caml/bigarray.h>
 #include <caml/fail.h>
 #include <caml/memory.h>
@@ -23,5 +24,61 @@ void hdf5_h5lt_make_dataset(value loc_id_v, value dset_name_v, value dims_v,
     H5T_val(type_id_v), Caml_ba_data_val(buffer_v));
   free(dims);
   raise_if_fail(err);
+  CAMLreturn0;
+}
+
+void hdf5_h5lt_read_dataset_int(value loc_id_v, value dset_name_v, value buffer_v)
+{
+  CAMLparam3(loc_id_v, dset_name_v, buffer_v);
+  raise_if_fail(H5LTread_dataset_int(Loc_val(loc_id_v), String_val(dset_name_v),
+    Caml_ba_data_val(buffer_v)));
+  CAMLreturn0;
+}
+
+value hdf5_h5lt_get_dataset_info(value loc_id_v, value dset_name_v)
+{
+  CAMLparam2(loc_id_v, dset_name_v);
+  CAMLlocal1(info);
+  hid_t loc_id = Loc_val(loc_id_v);
+  const char *dset_name = String_val(dset_name_v);
+  int rank;
+  hsize_t *dims;
+  H5T_class_t class_id;
+  size_t type_size;
+  herr_t err;
+  
+  raise_if_fail(H5LTget_dataset_ndims(loc_id, dset_name, &rank));
+  dims = calloc(rank, sizeof(hsize_t));
+  if (dims == NULL)
+    caml_raise_out_of_memory();
+  err = H5LTget_dataset_info(loc_id, dset_name, dims, &class_id, &type_size);
+  if (err < 0)
+  {
+    free(dims);
+    fail();
+  }
+  info = caml_alloc_tuple(3);
+  Store_field(info, 0, val_hsize_t_array(rank, dims));
+  Store_field(info, 1, Val_h5t_class(class_id));
+  Store_field(info, 2, Val_int(type_size));
+  CAMLreturn(info);
+}
+
+void hdf5_h5lt_set_attribute_int(value loc_id_v, value obj_name_v, value attr_name_v,
+  value buffer_v)
+{
+  CAMLparam4(loc_id_v, obj_name_v, attr_name_v, buffer_v);
+  raise_if_fail(H5LTset_attribute_int(Loc_val(loc_id_v), String_val(obj_name_v),
+    String_val(attr_name_v), Caml_ba_data_val(buffer_v),
+    Caml_ba_array_val(buffer_v)->dim[0]));
+  CAMLreturn0;
+}
+
+void hdf5_h5lt_get_attribute_int(value loc_id_v, value obj_name_v, value attr_name_v,
+  value data_v)
+{
+  CAMLparam4(loc_id_v, obj_name_v, attr_name_v, data_v);
+  raise_if_fail(H5LTget_attribute_int(Loc_val(loc_id_v), String_val(obj_name_v),
+    String_val(attr_name_v), Caml_ba_data_val(data_v)));
   CAMLreturn0;
 }
