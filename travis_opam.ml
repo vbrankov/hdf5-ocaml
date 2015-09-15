603c1a375bc2e4799067342157b2c7f23c033829
Vladimir Brankov <vbrankov@janestreet.com>
2015-09-15 16:28:54 -0400

    Another experiment.

--- travis_opam.ml
+++ travis_opam.ml
@@ -121,7 +121,7 @@ List.iter pin pins;
 
 (* Install the external dependencies *)
 ?|~ "sudo apt-get update";
-?|~ "sudo apt-get install libhdf5-serial-dev";
+?|~ "sudo apt-get -f install libhdf5-serial-dev";
 ?|~ "opam depext %s" pkg;
 
 (* Install the OCaml dependencies *)
