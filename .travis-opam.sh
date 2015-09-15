b50b6f4a645896ce634821f780a5838e2ac12c72
Vladimir Brankov <vbrankov@janestreet.com>
2015-09-15 15:51:56 -0400

    Another experiment.

--- .travis-opam.sh
+++ .travis-opam.sh
@@ -8,6 +8,19 @@ fork_branch=${FORK_BRANCH:-master}
 
 set -uex
 
+
+
+
+
+
+TMP_BUILD=$(mktemp -d 2>/dev/null || mktemp -d -t 'travistmpdir')
+
+cp .travis-ocaml.sh ${TMP_BUILD}
+cp yorick.mli ${TMP_BUILD}
+cp yorick.ml ${TMP_BUILD}
+cp travis_opam.ml ${TMP_BUILD}
+
+cd ${TMP_BUILD}
 sh .travis-ocaml.sh
 export OPAMYES=1
 eval $(opam config env)
@@ -19,5 +32,6 @@ ocamlc.opt yorick.mli
 ocamlfind ocamlc -c yorick.ml
 
 ocamlfind ocamlc -o travis-opam -package unix -linkpkg yorick.cmo travis_opam.ml
+cd -
 
-./travis-opam
+${TMP_BUILD}/travis-opam
