3cb0d4805308cfa8bf9405e85740397ee5b1eae6
Vladimir Brankov <vbrankov@janestreet.com>
2016-01-11 09:45:54 -0500

    Fix a typo.

--- lib/raw/h5tb.ml
+++ lib/raw/h5tb.ml
@@ -13,10 +13,10 @@ external make_table : string -> Hid.t -> string -> nrecords:int -> type_size:int
   = "hdf5_h5tb_make_table_bytecode" "hdf5_h5tb_make_table"
 external append_records : Hid.t -> string -> nrecords:int -> type_size:int
   -> field_offset:int array -> field_sizes:int array -> _ -> unit
-  = "hdf5_h5tb_append_records_bytecords" "hdf5_h5tb_append_records"
+  = "hdf5_h5tb_append_records_bytecode" "hdf5_h5tb_append_records"
 external write_records : Hid.t -> string -> start:int -> nrecords:int -> type_size:int
   -> field_offset:int array -> field_sizes:int array -> _ -> unit
-  = "hdf5_h5tb_write_records_bytecords" "hdf5_h5tb_write_records"
+  = "hdf5_h5tb_write_records_bytecode" "hdf5_h5tb_write_records"
 external read_table : Hid.t -> string -> dst_size:int -> dst_offset:int array
   -> dst_sizes:int array -> _ -> unit
   = "hdf5_h5tb_read_table_bytecode" "hdf5_h5tb_read_table"
